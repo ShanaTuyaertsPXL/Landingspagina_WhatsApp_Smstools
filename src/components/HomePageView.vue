f755fe4ad08c5db93ed04fda42ed193cf7a79d6e
ShanaTuyaertsPXL <93081695+ShanaTuyaertsPXL@users.noreply.github.com>
2023-03-28 22:37:51 +0200

    SCSS toegevoegd + component getest

--- src/components/HomePageView.vue
+++ src/components/HomePageView.vue
@@ -1,5 +1,7 @@
 <template>
 
+  <div id="div"><h1 class="titel">Test titel</h1></div>
+
 </template>
 
 <script>
@@ -8,6 +10,17 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+$bgColor: red;
+$fontPrimary: 240px;
+
+#div {
+  background-color: $bgColor;
+
+  .titel {
+    font-size: $fontPrimary;
+    color: green;
+  }
+}
 
 </style>
